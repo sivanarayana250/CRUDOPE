454a38cf16df530a8ca15ca896267cb5bfc691e0
Sivanarayana333 <125660312+Sivanarayana333@users.noreply.github.com>
2023-02-28 13:17:17 +0530

    3 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,36 +1,28 @@
-import { Component, Output, OnInit, EventEmitter, } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 import { CommonService } from '../common.service';
 import { Employee } from '../employee.model';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  employess: any;
 
-
-employess:any;
-
-@Output() employeeData = new EventEmitter<any>();
-@Output() editData = new EventEmitter<any>();
-  constructor(private commonService: CommonService) { }
+  @Output() employeeData = new EventEmitter<any>();
+  @Output() editData = new EventEmitter<any>();
+  constructor(private commonService: CommonService) {}
 
   ngOnInit(): void {
     this.employess = this.commonService.EMPLOYEE_DATA;
   }
 
-
-
-  public deleteEmployee(i: number) {
-    this.employeeData.emit(i);
+  public deleteEmployee(employee: Employee, index: number) {
+    this.employeeData.emit({ employee: employee, index: index });
   }
 
-  public EditEmployee( employee:Employee , index: number) {
-    this.editData.emit({employee: employee, index: index});
+  public EditEmployee(employee: Employee, index: number) {
+    this.editData.emit({ employee: employee, index: index });
   }
-
-
 }
-
-
